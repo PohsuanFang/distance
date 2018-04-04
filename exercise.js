802c711e07a6dabede90a9caadb483724d416e3d
PohsuanFang <phfang@mit.edu>
2018-04-04 10:31:23 -0400

    first commit

--- exercise.js
+++ exercise.js
@@ -5,7 +5,14 @@ exercise.distance = function(p1,p2){
     //-------------------
     //---- Your Code ----
     //-------------------    
-    return 'Error: 1st function not implemented';    
+    var x1=p1.x;
+    var y1=p1.y;
+    var x2=p2.x;
+    var y2=p2.y;
+    var dx=x2-x1;
+    var dy=y2-y1;
+    var d=Math.sqrt(dx*dx+dy*dy);
+    return d;    
 };
 
 // calculate distance between new point and existing
@@ -13,7 +20,13 @@ exercise.distances = function(newPoint,data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+    var distances=[];
+    data.forEach(function(item){
+        var dx=newPoint.x-item.x;
+    var dy=newPoint.y-item.y;
+    item.distance=Math.sqrt(dx*dx+dy*dy);  
+    });
+    return data;
 };
 
 // find k closest points
@@ -21,7 +34,11 @@ exercise.findClosest = function(k,data){
     //-------------------
     //---- Your Code ----
     //------------------- 
-    return 'Error: 3rd function not implemented';    
+    data.sort(function(a,b){
+        return a.distance-b.distance;
+    });
+
+    return data.slice(0,k);    
 };
 
 module.exports = exercise;
